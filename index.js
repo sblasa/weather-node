ba8770be8772701961e0096027dab0aac29c4b35
sblasa <msblasa@gmail.com>
2017-11-06 19:48:59 -0800

    add index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+let request = require('request');
+
+let apiKey = 'db0d02be7935a6bdd3d3bba84ea28d54';
+let city = 'diamond bar';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+    if(err){
+        console.log('error:', error);
+    } else {
+        console.log('body:', body);
+        
+    }
+});
---
+++ test.js
@@ -0,0 +1,13 @@
+let request = require('request');
+
+let apiKey = 'db0d02be7935a6bdd3d3bba84ea28d54';
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
